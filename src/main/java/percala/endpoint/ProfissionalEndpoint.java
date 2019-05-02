123e6e1e5dafbfa73ebdb68036d46bb84448044c
Allan Krueger <klimber.mail@gmail.com>
2019-05-02 16:21:12 -0300

    Refactor endpoint

--- src/main/java/percala/endpoint/ProfissionalEndpoint.java
+++ src/main/java/percala/endpoint/ProfissionalEndpoint.java
@@ -1,17 +1,31 @@
-package percala.controller;
+package percala.endpoint;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import percala.model.Profissional;
+import percala.repository.ProfissionalRepository;
 
 @RestController
 public class ProfissionalEndpoint {
+
+	@Autowired
+	private ProfissionalRepository ProfissionalRepository;
+
+	@PostMapping("/profissional")
+	public Profissional insereProfissional(@RequestParam("nome") String nome, @RequestParam("cpf") Long cpf) {
+
+		return ProfissionalRepository.save(new Profissional(nome, cpf));
+
+	}
 	
-	@RequestMapping("/profissional")
-	public Profissional insereProfissional(@RequestParam(value="nome", defaultValue="João") String nome, @RequestParam(value="cpf", defaultValue="00000000000") Long cpf) {
-		return new Profissional(nome, cpf);
+	@GetMapping("/profissional/{id}")
+	public Profissional mostraProfissional(@RequestParam("id") Long id) {
+		
+		return ProfissionalRepository.getOne(id);
 	}
 
 }
---
+++ src/main/java/percala/repository/ProfissionalRepository.java
@@ -0,0 +1,9 @@
+package percala.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import percala.model.Profissional;
+
+public interface ProfissionalRepository extends JpaRepository<Profissional, Long> {
+
+}
